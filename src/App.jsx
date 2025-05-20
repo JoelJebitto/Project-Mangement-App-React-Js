b3bc266331c36274efdb456ab24a4cc708410c5c
Joel Jebitto <joeljebitto@gmail.com>
2025-05-20 21:57:02 +0530

    add project completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import MainSection from "./Sections/MainSection";
 import NavBar from "./Sections/NavBar";
 import { useEffect } from "react";
 
-
 function App() {
   const [data, setData] = useState([]);
   const [main, setMain] = useState(null);
@@ -28,37 +27,30 @@ function App() {
   };
 
   const removeProject = (id) => {
-    const temp = data;
+    let temp = data;
     temp = temp.filter((i) => i.id != id);
     setData(temp);
   };
 
   const addTodo = (id, todo) => {
-    const temp = data;
+    let temp = data;
     temp[id].todo.push(todo);
     setData(temp);
   };
 
   const removeTodo = (id, todo) => {
-    const temp = data;
+    let temp = data;
     temp[id].todo = data[id].todo.filter((item) => item !== todo);
     setData(temp);
   };
 
   useEffect(() => {
-
-    addProject("Hi", "hello", "...")
-  }, [main])
+    console.log(data);
+  }, [main]);
   return (
     <div className="flex h-screen">
-      <NavBar
-        project={data}
-        setPro={setPro}
-        main={setMain}
-      />
+      <NavBar project={data} setPro={setPro} main={setMain} />
       <MainSection main={main} setMain={setMain} addProject={addProject} />
-
-
     </div>
   );
 }
--- src/MainSection/ProjectCreate.jsx
+++ src/MainSection/ProjectCreate.jsx
@@ -1,36 +1,77 @@
-const ProjectCreate = ({ setMain }) => {
-  // const
+import { useRef } from "react";
 
+const ProjectCreate = ({ setMain, addProject }) => {
+  const title = useRef("");
+  const description = useRef("");
+  const dob = useRef("");
+  const save = () => {
+    if (title.current.value != "") {
+      addProject(
+        title.current.value,
+        description.current.value,
+        dob.current.value,
+      );
+      setMain(null);
+    } else {
+      alert("Enter The Title");
+    }
+  };
   return (
     <div className="flex flex-col h-full">
-      <h1 className="text-5xl text-center font-bold my-10 mt-[20%] text-gray-100 ">Add A New Project</h1>
+      <h1 className="text-5xl text-center font-bold my-10 mt-[20%] text-gray-100 ">
+        Add A New Project
+      </h1>
 
       <div className="flex-1  px-20 flex h-auto flex-col">
-        <label htmlFor="title" className="text-xl py-1">Title :</label>
-        <input type="text" id="title" className="rounded-2xl p-2 px-4 mb-10 focus:ring-0 focus:outline-none" />
+        <label htmlFor="title" className="text-xl py-1">
+          Title :
+        </label>
+        <input
+          ref={title}
+          type="text"
+          id="title"
+          className="rounded-2xl p-2 px-4 mb-10 focus:ring-0 focus:outline-none"
+        />
 
-        <label htmlFor="description" className="text-xl py-1 ">Description :</label>
-        <textarea rows={5} type="text" id="description" className="rounded-2xl py-3 px-4 mb-10 focus:ring-0 focus:outline-none" />
+        <label htmlFor="description" className="text-xl py-1 ">
+          Description :
+        </label>
+        <textarea
+          ref={description}
+          rows={5}
+          type="text"
+          id="description"
+          className="rounded-2xl py-3 px-4 mb-10 focus:ring-0 focus:outline-none"
+        />
 
-        <label htmlFor="date" className="text-xl py-1 ">DOB :</label>
-        <input type="date" name="" id="date" className="rounded-2xl p-2 px-4 mb-10 focus:ring-0 focus:outline-none" />
-
+        <label htmlFor="date" className="text-xl py-1 ">
+          DOB :
+        </label>
+        <input
+          ref={dob}
+          type="date"
+          name=""
+          id="date"
+          className="rounded-2xl p-2 px-4 mb-10 focus:ring-0 focus:outline-none"
+        />
 
         <div className="flex flex-row-reverse w-full">
-          <button className=" mx-5 bg-stone-300 active:bg-slate-50 hover:bg-stone-200 transition-all text-gray-950 p-4 px-6 rounded-3xl mt-7">
-            {" "} Save{" "}
+          <button
+            onClick={save}
+            className=" mx-5 bg-stone-300 active:bg-slate-50 hover:bg-stone-200 transition-all text-gray-950 p-4 px-6 rounded-3xl mt-7"
+          >
+            {" "}
+            Save{" "}
           </button>
           <button
             onClick={() => setMain(null)}
-            className=" mx-5 bg-gray-300 active:bg-slate-50 hover:bg-gray-200 transition-all text-gray-950 p-4 px-6 rounded-3xl mt-7">
+            className=" mx-5 bg-gray-300 active:bg-slate-50 hover:bg-gray-200 transition-all text-gray-950 p-4 px-6 rounded-3xl mt-7"
+          >
             Cancel
           </button>
         </div>
       </div>
-
-
-
-    </div >
+    </div>
   );
 };
 
