c926b928e265bc69210d2b3933633afe049267c1
Joel Jebitto <joeljebitto@gmail.com>
2025-05-20 22:14:09 +0530

    No Project Page Added

--- src/MainSection/Default.jsx
+++ src/MainSection/Default.jsx
@@ -1,7 +1,20 @@
-const Default = () => {
+import image from "../assets/no-projects.png";
+
+const Default = ({ setMain }) => {
   return (
-    <div>Default</div>
-  )
+    <div className="flex flex-col x-full">
+      <img src={image} className="mx-auto mt-[25%] h-52 w-52" alt="" />
+      <p className="text-center pt-10 text-white">
+        Select a Project or get started with a new one
+      </p>
+      <button
+        onClick={() => setMain(0)}
+        className="bg-gray-300 active:bg-slate-50 hover:bg-gray-200 mx-auto transition-all w-[50%] text-gray-950 p-4 rounded-3xl mt-7"
+      >
+        Create New Project
+      </button>
+    </div>
+  );
 };
 
 export default Default;
